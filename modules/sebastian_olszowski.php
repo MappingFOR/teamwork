d0534c7e2c1f60b77ea6f36ddbb6d8d7c80fa495
Sebastian Olszowski <creesee@gmail.com>
2016-01-20 16:18:50 +0100

    Dodanie własnego modułu

---
+++ modules/sebastian_olszowski.php
@@ -0,0 +1,11 @@
+<?php
+
+	class SEBASTIAN_OLSZOWSKI extends MODULE {
+		public function register() {
+			echo 'Zostałem załadowany do pamięci';
+		}
+		
+		public function run() {
+			echo 1;
+		}
+	}
